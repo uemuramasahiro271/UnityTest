e4e516b82752ac6e1ec13d012bfa4404e8887958
uemuramasahiro271 <uemura.free2020@gmail.com>
2020-08-01 21:38:30 +0900

    初回コミット

---
+++ Assets/Scripts/StartShot.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class StartShot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.eulerAngles = new Vector3(0, Random.Range(30, 120), 0);
+        gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * 500);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
